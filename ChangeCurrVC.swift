0abaef33092b61920ec57e2c59c352385f890317
Pavel Isakov <81164077+ispawel@users.noreply.github.com>
2022-10-03 21:34:40 +0700

    Update ChangeCurrVC.swift

--- ChangeCurrVC.swift
+++ ChangeCurrVC.swift
@@ -15,32 +15,18 @@ open class ChangeCurrVC: UIViewController {
    
     //Пикер
     @IBOutlet weak var pickerView: UIPickerView!
-    
    
     //текстовое поля типа Валюты
     @IBOutlet weak var currTextField: UITextField!
-    
-    
-    
     let currArray = ["Австралийский доллар","Азербайджанский манат","Фунт стерлингов Соединенного королевства","Армянских драмов","Белорусский рубль","Болгарский лев","Бразильский реал","Венгерских форинтов","Гонконгских долларов","Датская крона","Доллар США","Евро","Индийских рупий","Казахстанских тенге","Канадский доллар","Киргизских сомов","Китайский юань","Молдавских леев","Норвежских крон","Польский злотый","Румынский лей","СДР (специальные права заимствования)","Сингапурский доллар","Таджикских сомони","Турецких лир","Новый туркменский манат","Узбекских сумов","Украинских гривен","Чешских крон","Шведских крон","Швейцарский франк","Южноафриканских рэндов","Вон Республики Корея","Японских иен"]
-    
-    
-    
-    
     open override func viewDidLoad() {
         super.viewDidLoad()
-        
         inLabel.text = textOfInLabelText
-
         pickerView.dataSource = self
         pickerView.delegate = self
-        
         //вызов метода скрытия клавиатуры
         addTapGestureToHideKeyboard()
     }
-    
-    
-    
     //функция передачи данных между view
     open override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC: CalcVC = segue.destination as! CalcVC
@@ -48,14 +34,8 @@ open class ChangeCurrVC: UIViewController {
         //Присваивание значения в окне третьем view
         destinationVC.textOfInLabelCalc = inLabel.text!
         
-      destinationVC.textOfInTypeCurrLabel = currTextField.text!
-        
-        
+      destinationVC.textOfInTypeCurrLabel = currTextField.text! 
     }
-    
-    
-    
-    
     //Метод скрытия клавиатуры
     func addTapGestureToHideKeyboard() {
             let tapGesture = UITapGestureRecognizer(target: view, action: #selector(view.endEditing))
@@ -64,36 +44,24 @@ open class ChangeCurrVC: UIViewController {
     func tapGesture() {
     //        currTextField.resignFirstResponder()
         }
-    
-    
-    
     //переход на окно расчета
     @IBAction func toCalcBtn(_ sender: UIButton) {
-    }
-    
+    } 
 }
-
-
-
 // Подключение протокола UIPicker через раширение класса
 extension ChangeCurrVC:UIPickerViewDataSource,UIPickerViewDelegate{
-
-     
     //количесво компонентов
     public func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-
     //Колличество элементов в компоненте
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
        return 34
     }
-    
     //метод установки заголовка
     public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return currArray[row]
     }
-    
     //метод выбора активного значения пикера
     public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
       currTextField.text = currArray[row]
